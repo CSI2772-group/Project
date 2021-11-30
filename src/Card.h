46679ae605518b10f22aa2983eb6329425ef99c4
Fernando Nogueira <abert036@uottawa.ca>
2021-11-29 19:23:46 -0500

    refactor: make card functions static

--- src/Card.h
+++ src/Card.h
@@ -12,31 +12,32 @@
 // I really don't want to type extra 160 loc
 // because of the inheritance requirements
 
-#define CARD_CLASS(name, chr, a, b, c, d)                      \
-    class name : public Card                                   \
-    {                                                          \
-    public:                                                    \
-        name() {}                                              \
-        ~name() override = default;                            \
-        void print(std::ostream &out) override { out << chr; } \
-        std::string getName() override { return #name; }       \
-        int getCardsPerCoin(int coins) override                \
-        {                                                      \
-            switch (coins)                                     \
-            {                                                  \
-            case 1:                                            \
-                return a;                                      \
-            case 2:                                            \
-                return b;                                      \
-            case 3:                                            \
-                return c;                                      \
-            case 4:                                            \
-                return d;                                      \
-            default:                                           \
-                return 0;                                      \
-            }                                                  \
-        }                                                      \
-        char cardType = chr;                                   \
+#define CARD_CLASS(name, chr, a, b, c, d)                           \
+    class name : public Card                                        \
+    {                                                               \
+    public:                                                         \
+        name() {}                                                   \
+        ~name() override = default;                                 \
+        static void const print(std::ostream &out)  { out << chr; } \
+        static std::string const getName()  { return #name; }       \
+        static char const getShortName()  { return chr; }           \
+        static int const getCardsPerCoin(int coins)                 \
+        {                                                           \
+            switch (coins)                                          \
+            {                                                       \
+            case 1:                                                 \
+                return a;                                           \
+            case 2:                                                 \
+                return b;                                           \
+            case 3:                                                 \
+                return c;                                           \
+            case 4:                                                 \
+                return d;                                           \
+            default:                                                \
+                return 0;                                           \
+            }                                                       \
+        }                                                           \
+        char cardType = chr;                                        \
     }
 
 // endregion
@@ -45,13 +46,15 @@
 
 class Card {
 public:
-    virtual ~Card() = default;;
+    virtual ~Card() = default;
 
-    virtual int getCardsPerCoin(int coins) = 0;
+    static const int getCardsPerCoin(int coins) { return 0; }
 
-    virtual std::string getName() = 0;
+    static const char getShortName() { return '\0'; }
 
-    virtual void print(std::ostream &out) = 0;
+    static const std::string getName() { return ""; }
+
+    static const void print(std::ostream &out) {};
 };
 
 CARD_CLASS(Blue, 'B', 4, 6, 8, 10);
@@ -78,6 +81,7 @@ class IllegalType : public std::exception {
 
 class CardFactory {
     friend class Table;
+
 public:
     void operator=(CardFactory const &) = delete;
 
@@ -88,33 +92,43 @@ public:
         return &factory;
     }
 
-
     Card *makeCard(char cardType) {
         Card *card;
-        if (cardType == 'B')
-            card = new Blue();
-        else if (cardType == 'C')
-            card = new Chili();
-        else if (cardType == 'S')
-            card = new Stink();
-        else if (cardType == 'G')
-            card = new Green();
-        else if (cardType == 's')
-            card = new Soy();
-        else if (cardType == 'b')
-            card = new Black();
-        else if (cardType == 'R')
-            card = new Red();
-        else if (cardType == 'g')
-            card = new Garden();
-        else
-            throw IllegalType();
+        switch (cardType) {
+            case 'B':
+                card = new Blue();
+                break;
+            case 'C':
+                card = new Chili();
+                break;
+            case 'S':
+                card = new Stink();
+                break;
+            case 'G':
+                card = new Green();
+                break;
+            case 's':
+                card = new Soy();
+                break;
+            case 'b':
+                card = new Black();
+                break;
+            case 'R':
+                card = new Red();
+                break;
+            case 'g':
+                card = new Garden();
+                break;
+            default:
+                throw IllegalType();
+        }
         cards.push_back(card);
         return card;
     }
 
 private:
     CardFactory() = default;
+
     std::vector<Card *> cards;
 };
 
