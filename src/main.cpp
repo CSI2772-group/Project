4e4f8497192403eda35167da5d466cba7e871b90
Fernando Nogueira <abert036@uottawa.ca>
2021-12-06 17:31:10 -0500

    feat: add most of the main logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,50 @@
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 #include "Table.h"
 #include "Utils.h"
 
-const std::string beanAsciiArt =
-        "      ████████\n"
-        "    ██▒▒▒▒▒▒▒▒▒▒██\n"
-        "  ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒██\n"
-        "██▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒██\n"
-        "██▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒██\n"
-        "██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████\n"
-        "██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██\n"
-        "████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██\n"
-        "    ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██\n"
-        "      ████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██\n"
-        "          ████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██\n"
-        "              ████████████████\n";
+const std::string beanAsciiArt = "      ████████\n"
+                                 "    ██▒▒▒▒▒▒▒▒▒▒██\n"
+                                 "  ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒██\n"
+                                 "██▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒██\n"
+                                 "██▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒██\n"
+                                 "██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████\n"
+                                 "██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██\n"
+                                 "████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██\n"
+                                 "    ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██\n"
+                                 "      ████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██\n"
+                                 "          ████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██\n"
+                                 "              ████████████████\n";
 
-
-int main() {
-    Table* table;
+int main()
+{
+    Table *table;
 
     // Print corny ascii art  🌽
     std::cout << beanAsciiArt;
-    std::cout << "Welcome to the Bean Game!\nLiterally Fall Guys™ except its like an obscure card game :/\n";
-
+    std::cout << "Welcome to the Bean Game!\nLiterally Fall Guys™ except its "
+                 "like an obscure card game :/\n";
 
     // region Save or Load
 
     // Ask if user wants to load a game or start a new one
     std::ifstream save("save.bean");
     bool loaded = false;
-    if (save.is_open()) {
-        std::cout << "Save file found! Do you want to load it? (y/N)"
-                  << std::endl;
+    if (save.is_open())
+    {
+        std::cout << "Save file found! Do you want to load it? (y/N)" << std::endl;
         char answer;
         std::cin >> answer;
-        if (answer == 'y') {
-            table = Utils::loadGame(save);
+        if (answer == 'y')
+        {
+            table = new Table(save, CardFactory::getFactory());
             loaded = true;
         }
     }
-    if (!loaded){
+    if (!loaded)
+    {
         std::string p2Name;
         std::cout << "Starting a new game!" << std::endl;
 
@@ -51,11 +52,15 @@ int main() {
         std::cout << "Player 1, what is your name?" << std::endl;
         std::string p1Name = Utils::getLine(3);
         std::cout << "Player 2, what is your name?" << std::endl;
-        while (true) {
+        while (true)
+        {
             p2Name = Utils::getLine(3);
-            if (p2Name != p1Name) {
+            if (p2Name != p1Name)
+            {
                 break;
-            } else {
+            }
+            else
+            {
                 std::cout << "Player 2, please choose a different name!" << std::endl;
             }
         }
@@ -67,11 +72,13 @@ int main() {
     // endregion
 
     // region Main Game Loop
-    Player* currentPlayer = table->getCurrentPlayer();
-    while(!table->deck.empty()){
+    Player *currentPlayer = table->getCurrentPlayer();
+    while (!table->deck.empty())
+    {
         //          Put 3 cards from top deck into trade area
         int drawnCount = 0;
-        while(drawnCount < 3 && !table->deck.empty()){
+        while (drawnCount < 3 && !table->deck.empty())
+        {
             table->tradeArea.cards.push_back(table->deck.back());
             table->deck.pop_back();
             drawnCount++;
@@ -81,92 +88,109 @@ int main() {
         //        while(discard_pile.top() in trade area)
         //            trade area.add(discard_pile.pop())
         bool inTradeArea = true;
-        while(inTradeArea){
+        while (inTradeArea)
+        {
             inTradeArea = false;
-            Card* topDiscard = table->discardPile.top();
-            for(Card* card : table->tradeArea.cards){
-                if(card->getName() == topDiscard->getName()){
-                    inTradeArea = true;
-                    table->tradeArea.cards.push_back(topDiscard);
-                    table->discardPile.pop_back();
-                    break;
+            if (!table->discardPile.empty()){
+                Card *topDiscard = table->discardPile.top();
+                for (Card *card : table->tradeArea.cards)
+                {
+                    if (card->getName() == topDiscard->getName())
+                    {
+                        inTradeArea = true;
+                        table->tradeArea.cards.push_back(topDiscard);
+                        table->discardPile.pop_back();
+                        break;
+                    }
                 }
             }
         }
 
-        //  TODO: Auto add trade cards to chains (later discard them)
-
+        //  TODO: (Optional) Auto add trade cards to chains (later discard them)
 
         // TODO: Ask user input here
         Utils::clearScreen();
         Utils::printTable(table);
 
         //  player.play_top_card()
-        Card* card = currentPlayer->hand.top();
-        // TODO: Force user to use card
-
+        currentPlayer->plantTop();
 
         // while (player don't want to end turn)
         //        ask for:
+        //            - save and quit // once
+        //             - end turn  // once
         //            - player.play_top_card() // once
         //            - player.discard_any()  // once
+        //            - player.buy_third_chain() // once
         //            - trade from trade area // unlimited
         bool playedTwice = false;
         bool discarded = false;
-        while (true){
-            // Ask for input
-
-            // TODO: Implement the saving stuff
-
+        while (true)
+        {
             std::cout << "[e]nd turn\t";
+            std::cout << "[s]ave and quit\t"; // TODO: Implement the saving stuff
+            std::cout << "[t]rade\t";         // TODO: trade from trade area
             if (!playedTwice)
                 std::cout << "[p]lay again\t";
             if (!discarded)
                 std::cout << "[d]iscard\t";
+            if (currentPlayer->getNumChains() < 3)
+            {
+                std::cout << "[b]uy third chain\t";
+            }
             std::cout << std::endl;
 
             char input;
             std::cin >> input;
 
             bool doneWithTurn = false;
-            switch(input){
-                case 'e':
-                    doneWithTurn = true;
-                    std::cout << "Ending turn" << std::endl;
+            switch (input)
+            {
+            case 'e':
+                doneWithTurn = true;
+                std::cout << "Ending turn.." << std::endl;
+                break;
+            case 'p': // Plant again
+                if (playedTwice)
+                {
+                    std::cout << "You can only play twice per turn!" << std::endl;
                     break;
-                case 'p':
-                    if (playedTwice){
-                        std::cout << "You can only play twice per turn!" << std::endl;
-                        break;
-                    }
-                    playedTwice = true;
-                    // TODO: Play again logic
+                }
+                playedTwice = true;
+                currentPlayer->plantTop();
+                break;
+            case 'd':
+                if (discarded)
+                {
+                    std::cout << "You can only discard once per turn!" << std::endl;
                     break;
-                case 'd':
-                    if (discarded){
-                        std::cout << "You can only discard once per turn!" << std::endl;
-                        break;
-                    }
-                    discarded = true;
-                    // TODO: Discard one card from hand logic
-                    break;
-                default:
-                    std::cout << "Invalid input!" << std::endl;
-                    break;
+                }
+                discarded = true;
+                currentPlayer->discardAny();
+                break;
+            case 'b':
+                currentPlayer->buyThirdChain();
+                break;
+            default:
+                std::cout << "Invalid input!" << std::endl;
+                break;
             }
 
-            if (doneWithTurn){
+            if (doneWithTurn)
+            {
                 break;
             }
         }
 
-        // TODO: Put two cards from deck into player
-
-        //  end turn
-
+        // Put two cards from deck into player
+        currentPlayer->hand.push_back(table->deck.back());
+        table->deck.pop_back();
+        currentPlayer->hand.push_back(table->deck.back());
+        table->deck.pop_back();
 
         // Flip player's turn
         table->p1Turn = !table->p1Turn;
+        currentPlayer = table->getCurrentPlayer();
     }
 
     // endregion
@@ -177,4 +201,3 @@ int main() {
 
     return 0;
 }
-
